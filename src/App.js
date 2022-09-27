9694d81e3a6b0492ddec41ef1a0368053207317a
iinaamasum <iinaamasum@gmail.com>
2022-09-27 22:22:18 +0600

    classes card added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/Shared/NavBar';
 import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
+import Classes from './Pages/Classes/Classes';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Classes />} />
       </Routes>
       <Toaster position="top-right" reverseOrder={false} />
     </>
