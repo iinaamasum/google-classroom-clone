91cc590b3b29959f458cbd95ccd450587ea88acd
iinaamasum <iinaamasum@gmail.com>
2022-10-03 11:53:39 +0600

    file upload with new class work done

--- src/Pages/ClassDashboard/CreateClassWork.jsx
+++ src/Pages/ClassDashboard/CreateClassWork.jsx
@@ -6,16 +6,79 @@ import {
   Tooltip,
   Typography,
 } from '@material-tailwind/react';
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { FiUpload } from 'react-icons/fi';
 import { MdCancel } from 'react-icons/md';
+import LoadingComponent from '../../components/Shared/LoadingComponent';
+import auth from '../../firebase.init';
 
 const CreateClassWork = ({ createWork, setCreateWork, classId }) => {
   const [workFile, setWorkFile] = useState(null);
+  const { handleSubmit, register } = useForm();
+  const [user, userLoading] = useAuthState(auth);
+
+  if (userLoading) {
+    return <LoadingComponent />;
+  }
+
+  const onSubmit = async (data) => {
+    if (data.workTitle === '') {
+      toast.error('Work Title not provided.');
+      return;
+    }
+    if (data.workDetails === '') {
+      toast.error('Work details should be filled.');
+      return;
+    }
+
+    let classWorkResponse;
+    try {
+      const formData = new FormData();
+      formData.append('workFile', workFile);
+      classWorkResponse = await axios
+        .post('http://localhost:5001/api/v1/class-work/file-upload', formData)
+        .then((res) => res.data);
+      console.log('first', classWorkResponse);
+    } catch (error) {
+      toast.error(error.message);
+      return;
+    }
+    if (!classWorkResponse) return;
+    const { filename, path } = classWorkResponse.file;
+
+    try {
+      const postingWorkData = {
+        ...data,
+        workFileName: filename,
+        path,
+        classId,
+        userEmail: user.email,
+      };
+      console.log(postingWorkData);
+      const uploadClassWorkWithFile = await axios
+        .post('http://localhost:5001/api/v1/class-work', postingWorkData)
+        .then((res) => res.data);
+      console.log(uploadClassWorkWithFile);
+    } catch (error) {
+      toast.error(error.message);
+    }
+
+    setCreateWork(!createWork);
+  };
+
   const handleFileChange = (e) => {
+    if (e.target.files[0].size > 10000000) {
+      toast.error(
+        'File is too long to save into the server. Please a select a file less than 10MB.'
+      );
+      return;
+    }
     setWorkFile(e.target.files[0]);
   };
-  console.log(classId);
   return (
     <section>
       <Card
@@ -24,33 +87,39 @@ const CreateClassWork = ({ createWork, setCreateWork, classId }) => {
         }}
         className="p-4"
       >
-        <div className="">
-          <Typography variant="h5" className="mt-2 mb-3">
-            Announcement or class work creation form
-          </Typography>
+        <Typography variant="h5" className="mt-2 mb-3">
+          Announcement or class work creation form
+        </Typography>
+        <form
+          id="create-work-form"
+          onSubmit={handleSubmit(onSubmit)}
+          className=""
+        >
           <div className="">
-            <div className="">
+            <div className="relative">
               <label htmlFor="">Title</label>
               <Input
                 variant="standard"
                 className="bg-gray-50 w-full focus:border-0 focus:outline-none focus:ring-0 px-3"
                 placeholder="Announcement title"
+                name="workTitle"
+                {...register('workTitle')}
               />
             </div>
-            <div className="my-3">
+            <div className="my-3 relative">
               <label htmlFor="">Details</label>
               <Textarea
-                name=""
                 color="blue"
                 variant="standard"
                 className="bg-gray-50 w-full focus:border-0 focus:outline-none"
                 placeholder="Write down the announcement body"
+                {...register('workDetails')}
               />
             </div>
           </div>
-        </div>
+        </form>
         {workFile && (
-          <div className="flex gap-x-10 items-center border-[1px] justify-center py-5">
+          <div className="flex gap-x-5 items-center border-[1px] justify-center py-5 bg-gray-50 rounded-lg">
             <Tooltip content="Uploaded File Name">
               <p className="text-xl">{workFile.name}</p>
             </Tooltip>
@@ -99,6 +168,8 @@ const CreateClassWork = ({ createWork, setCreateWork, classId }) => {
               Cancel
             </Button>
             <Button
+              type="submit"
+              form="create-work-form"
               className="h-[30px] min-h-6 px-5 capitalize text-[15px] inline-flex items-center justify-center"
               variant="filled"
               color="indigo"
